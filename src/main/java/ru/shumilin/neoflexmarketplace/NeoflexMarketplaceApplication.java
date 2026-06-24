c5621cd8d0ed6fd5f745ce9cd5743d024ee6823f
danil <danilsuperworld@gmail.com>
2026-06-24 17:43:34 +0400

    Initial commit

---
+++ src/main/java/ru/shumilin/neoflexmarketplace/NeoflexMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package ru.shumilin.neoflexmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoflexMarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/shumilin/neoflexmarketplace/NeoflexMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shumilin.neoflexmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexMarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
