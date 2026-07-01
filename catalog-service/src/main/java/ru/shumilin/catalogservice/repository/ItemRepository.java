d25a392395b0c08ea1b08b5434c9973ae0dcaf26
danil <danilsuperworld@gmail.com>
2026-07-01 14:46:02 +0400

    Add findAllByName method to ItemRepository

--- catalog-service/src/main/java/ru/shumilin/catalogservice/repository/ItemRepository.java
+++ catalog-service/src/main/java/ru/shumilin/catalogservice/repository/ItemRepository.java
@@ -1,9 +1,32 @@
 package ru.shumilin.catalogservice.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import ru.shumilin.catalogservice.entity.ItemEntity;
+import ru.shumilin.catalogservice.model.entity.ItemEntity;
+import ru.shumilin.catalogservice.model.projection.ItemWithSupplierProjection;
+
 
 @Repository
 public interface ItemRepository extends CrudRepository<ItemEntity, Integer> {
+    @Query(value = """
+            SELECT i.id, i.name, i.price, o.name AS supplier_name, i.quantity
+            FROM CATALOG.item i
+            JOIN users.organization o ON i.id_org_supplier = o.id
+            WHERE i.name ILIKE CONCAT('%', :name, '%')
+                        AND (:categoryId IS NULL OR i.id_category = :categoryId)
+            """,
+            countQuery = """
+                    SELECT COUNT(*)
+                    FROM catalog.item i
+                    WHERE i.name ILIKE CONCAT('%', :name, '%')
+                                        AND (:categoryId IS NULL OR i.id_category = :categoryId)
+                    """,
+            nativeQuery = true)
+    Page<ItemWithSupplierProjection> findAllByName(@Param("name") String name,
+                                                   @Param("categoryId") Integer categoryId,
+                                                   Pageable pageable);
 }
