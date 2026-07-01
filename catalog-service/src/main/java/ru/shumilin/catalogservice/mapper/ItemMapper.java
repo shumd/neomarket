61b9b771c41c46f832ee8bfec15739a0ea55bef4
danil <danilsuperworld@gmail.com>
2026-07-01 15:09:29 +0400

    Add toPageResponseDto and toWithSupplierResponseDto methods to ItemMapper

--- catalog-service/src/main/java/ru/shumilin/catalogservice/mapper/ItemMapper.java
+++ catalog-service/src/main/java/ru/shumilin/catalogservice/mapper/ItemMapper.java
@@ -1,8 +1,12 @@
 package ru.shumilin.catalogservice.mapper;
 
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
+import ru.shumilin.catalogservice.dto.ItemPageResponseDto;
 import ru.shumilin.catalogservice.dto.ItemResponseDto;
-import ru.shumilin.catalogservice.entity.ItemEntity;
+import ru.shumilin.catalogservice.dto.ItemWithSupplierResponseDto;
+import ru.shumilin.catalogservice.model.entity.ItemEntity;
+import ru.shumilin.catalogservice.model.projection.ItemWithSupplierProjection;
 
 @Component
 public class ItemMapper { //Использовать mapStruct ?
@@ -16,6 +20,24 @@ public class ItemMapper { //Использовать mapStruct ?
                 entity.getCategoryId().toString(),
                 entity.getOrgSupplierId().toString(),
                 entity.getQuantity()
+        );
+    }
+
+    public ItemWithSupplierResponseDto toWithSupplierResponseDto(ItemWithSupplierProjection itemWithSupplierProjection) {
+        return new ItemWithSupplierResponseDto(
+                itemWithSupplierProjection.getId().toString(),
+                itemWithSupplierProjection.getName(),
+                itemWithSupplierProjection.getPrice(),
+                itemWithSupplierProjection.getSupplierName(),
+                itemWithSupplierProjection.getQuantity()
+        );
+    }
+
+    public ItemPageResponseDto toPageResponseDto(Page<ItemWithSupplierResponseDto> itemWithSupplierResponseDtoPage){
+        return new ItemPageResponseDto(
+                itemWithSupplierResponseDtoPage.getContent(),
+                itemWithSupplierResponseDtoPage.getSize(),
+                itemWithSupplierResponseDtoPage.getPageable().getOffset()
         );
     }
 }
