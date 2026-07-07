84ac92c6222fa1b0a228ed2db29f1129b9473d14
danil <danilsuperworld@gmail.com>
2026-07-07 17:25:37 +0400

    Add ErrorTitleConstant

---
+++ auth-service/src/main/java/ru/shumilin/authservice/util/ErrorTitleConstant.java
@@ -0,0 +1,6 @@
+package ru.shumilin.authservice.util;
+
+public class ErrorTitleConstant {
+    public static final String INTERNAL_SERVICE_ERROR = "Внутренняя ошибка сервера";
+    public static final String CONFLICT = "Конфликт данных";
+}
