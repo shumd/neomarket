5e6cf92b5111683a6d9bd044f595bccaadf4727d
danil <danilsuperworld@gmail.com>
2026-07-10 19:01:36 +0400

    Configure SecurityConfig

--- auth-service/src/main/java/ru/shumilin/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/ru/shumilin/authservice/config/SecurityConfig.java
@@ -1,24 +1,42 @@
 package ru.shumilin.authservice.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.shumilin.authservice.security.CustomAuthenticationEntryPoint;
+import ru.shumilin.authservice.security.JwtAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity,
+                                                   JwtAuthenticationFilter jwtAuthenticationFilter,
+                                                   CustomAuthenticationEntryPoint customAuthenticationEntryPoint){
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(e ->
+                        e.authenticationEntryPoint(customAuthenticationEntryPoint))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(authorizeHttpRequest ->
-                        authorizeHttpRequest.requestMatchers("/auth/**").permitAll()
+                        authorizeHttpRequest
+                                .requestMatchers("/auth/logout").authenticated()
+                                .requestMatchers("/auth/login",
+                                        "/auth/register").permitAll()
                                 .requestMatchers("/swagger-ui/**",
-                                        "/v3/api-docs/**").permitAll())
+                                        "/v3/api-docs/**").permitAll()
+                                .anyRequest().authenticated())
                 .build();
     }
 
