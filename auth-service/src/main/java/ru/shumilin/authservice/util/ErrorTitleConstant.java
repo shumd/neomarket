4284acde3f02ad01e41ddb37d5da41715350af0d
danil <danilsuperworld@gmail.com>
2026-07-10 18:32:37 +0400

    Change UNAUTHORIZED value

--- auth-service/src/main/java/ru/shumilin/authservice/util/ErrorTitleConstant.java
+++ auth-service/src/main/java/ru/shumilin/authservice/util/ErrorTitleConstant.java
@@ -5,5 +5,5 @@ public class ErrorTitleConstant {
     public static final String CONFLICT = "Конфликт данных";
     public static final String BAD_REQUEST = "Неверный формат данных";
     public static final String NOT_ACCEPTABLE = "Заголовок Accept не содержит application/json";
-    public static final String UNAUTHORIZED = "Неверный логин или пароль";
+    public static final String UNAUTHORIZED = "Необходима авторизация";
 }
